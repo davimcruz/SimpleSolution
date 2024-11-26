709d3e92f4e209eb6ca202d7d231386426fd281f
Maciel <112631220+macielrib@users.noreply.github.com>
2024-11-26 18:44:41 -0300

    Test fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Hero from "./(root)/page";
 import Header from "./components/header/page";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <Hero/>
     </>
   );
 }
