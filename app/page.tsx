a94918f5faecfa3bc5c8b304a723294f51eb955f
Maciel <112631220+macielrib@users.noreply.github.com>
2024-11-26 18:49:15 -0300

    Test hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import Header from "./components/header/page";
-
+import Hero from "./sections/Hero";
 export default function Home() {
   return (
     <>
+      <Hero />
       <Header />
     </>
   );
