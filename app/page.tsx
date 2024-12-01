b16433dda566b8e8bc2b8e037f6c89a62e8a0c7c
Maciel <kaueandretti@gmail.com>
2024-11-30 21:33:08 -0300

    - page.tsx para index.tsx em seções/componentes
    - Adicionado algumas seções

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import CompanySlider from "./components/companys/page";
-import Header from "./components/header/page";
-import AboutUs from "./sections/about/page";
-import Features from "./sections/features/page";
-import FeaturesFinal from "./sections/features/features-final/page";
-import Hero from "./sections/hero/page";
-import Ratings from "./sections/ratings/page";
+import CompanySlider from "./components/companys";
+import Header from "./components/header";
+import AboutUs from "./sections/about";
+import Features from "./sections/features";
+import FeaturesFinal from "./sections/features/features-final";
+import Ratings from "./sections/ratings";
+import Hero from "./sections/hero";
 export default function Home() {
   return (
     <>
